1c8040dfcff3e4a0468593f0542ec93b80c524ee
Pete Gallagher <pete@pjgcreations.co.uk>
2020-12-22 19:03:07 +0000

    Refactored

--- Program.cs
+++ Program.cs
@@ -15,6 +15,9 @@ namespace device_upload
 		static string DeviceConnectionString = "";
 		static DeviceClient Client = null;
 
+        static FileUploadSasUriResponse sasUploadUri = null;
+        static Uri fileUploadUri = null;
+
         static async Task Main(string[] args)
         {
             Console.WriteLine("*************************************************");
@@ -33,8 +36,6 @@ namespace device_upload
 				DeviceConnectionString = Console.ReadLine();
 				
 				InitClient();
-
-                await Client.SetReceiveMessageHandlerAsync(OnReceiveMessage, null);
 
                 await UploadFile();
                 
@@ -66,21 +67,29 @@ namespace device_upload
 
             var fileName = "test.csv";
 
-            using var fileContentsStream = new FileStream(fileName, FileMode.Open);
+            await GetFileUploadUri(fileName);
+            await UploadFileToBlobStorage(fileName);           
+            
+        }
 
+        private static async Task GetFileUploadUri(String fileName) {
             var fileUploadSasUriRequest = new FileUploadSasUriRequest();
             fileUploadSasUriRequest.BlobName = fileName;
 
             Console.WriteLine("Retrieving SAS URI for File Upload from the IoT Hub");
-            FileUploadSasUriResponse sasUploadUri = await Client.GetFileUploadSasUriAsync(fileUploadSasUriRequest);
+            sasUploadUri = await Client.GetFileUploadSasUriAsync(fileUploadSasUriRequest);
 
-            Uri fileUploadUri = new Uri($"https://{sasUploadUri.HostName}/{sasUploadUri.ContainerName}/{Uri.EscapeDataString(sasUploadUri.BlobName)}{sasUploadUri.SasToken}");
+            fileUploadUri = new Uri($"https://{sasUploadUri.HostName}/{sasUploadUri.ContainerName}/{Uri.EscapeDataString(sasUploadUri.BlobName)}{sasUploadUri.SasToken}");
 
             Console.WriteLine($"Successfully retrieved SAS URI ({fileUploadUri}) for File upload from IoT Hub");
+        }
+
+        private static async Task UploadFileToBlobStorage(String fileName) {
+            using var fileContentsStream = new FileStream(fileName, FileMode.Open);
 
             try
             {
-                Console.WriteLine($"Uploading file {fileName} using the Azure Storage SDK and the retrieved SAS URI for authentication");
+                Console.WriteLine($"Uploading file {fileName} to Blob Storage");
 
                 var blob = new CloudBlockBlob(fileUploadUri);
                 await blob.UploadFromStreamAsync(fileContentsStream);
@@ -89,17 +98,13 @@ namespace device_upload
             {
                 Console.WriteLine($"File Upload Failed {ex.ToString()} - Sending Failure Notification to IoT Hub");
 
-                var failedUploadNotification = new FileUploadCompletionNotification();
-
-                failedUploadNotification.CorrelationId = sasUploadUri.CorrelationId;
-                failedUploadNotification.IsSuccess = false;
-                failedUploadNotification.StatusCode = 500;
-                failedUploadNotification.StatusDescription = ex.Message;
-
-                await Client.CompleteFileUploadAsync(failedUploadNotification);
+                await SendFailureNotification(ex.Message);
             }
 
-            Console.WriteLine("File Uploaded Successfully - Sending Success Notification to IoT Hub");
+            Console.WriteLine("File Uploaded Successfully - Sending Success Notification to IoT Hub");           
+        }
+
+        private static async Task SendSuccessNotification() {
 
             var successfulUploadNotification = new FileUploadCompletionNotification();
 
@@ -108,29 +113,19 @@ namespace device_upload
             successfulUploadNotification.StatusCode = 200;
             successfulUploadNotification.StatusDescription = "Success";
 
-            await Client.CompleteFileUploadAsync(successfulUploadNotification);
-            
+            await Client.CompleteFileUploadAsync(successfulUploadNotification);            
         }
 
-        private static async Task GetFileUploadUri() {
+        private static async Task SendFailureNotification(String exceptionMessage) {
 
-        }
+            var failedUploadNotification = new FileUploadCompletionNotification();
 
+            failedUploadNotification.CorrelationId = sasUploadUri.CorrelationId;
+            failedUploadNotification.IsSuccess = false;
+            failedUploadNotification.StatusCode = 500;
+            failedUploadNotification.StatusDescription = exceptionMessage;
 
-        private static async Task OnReceiveMessage(Message message, object userContext)
-        {
-            var reader = new StreamReader(message.BodyStream);
-            var messageContents = reader.ReadToEnd();
-            Console.WriteLine($"Message Contents: {messageContents}");
-
-            Console.WriteLine("Message Propeties:");
-
-            foreach (var property in message.Properties)
-            {
-                Console.WriteLine($"Key: {property.Key}, Value: {property.Value}");
-            }
-
-            await Client.CompleteAsync(message);            
+            await Client.CompleteFileUploadAsync(failedUploadNotification);
         }
     }
 }
